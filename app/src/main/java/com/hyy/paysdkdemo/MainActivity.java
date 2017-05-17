3cb0005270181f967b153708448395daa7c4f12b
707058314@qq.com <>
2017-05-17 15:26:52 +0800

    Initial commit

---
+++ app/src/main/java/com/hyy/paysdkdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hyy.paysdkdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fancy.hyypaysdk.pay.wxpay.WXPayUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
